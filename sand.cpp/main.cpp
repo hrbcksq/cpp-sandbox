d5de8f4ae47989f9fa2c5115ee5c8a78673f8389
hrbkcsq <hrbcksq@gmail.com>
2017-03-25 01:21:28 +0200

    Namespaces

--- sand.cpp/main.cpp
+++ sand.cpp/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
 
 int main() {
-	using std::cout;
-	using std::endl;
-
-	cout << "Hello World!" << endl;
+	//cout << "Hello World!" << endl;
 	//<< std::endl
-	return 0;
+	return DemoConsoleOutput();
 }
+
+int DemoConsoleOutput() {
+	cout << "This is a simple string literal" << endl;
+	cout << "Writing number fie: " << 5 << endl;
+	cout << "Perfoming division 10 / 5 = " << 10 / 5 << endl;
+	cout << "Pi when approximated is 22 / 7 =" << 22 / 7 << endl;
+	cout << "Pi more accurately is 22 / 7 = " << 22.0 / 7 << endl;
+
+	return 0;
+} 
 

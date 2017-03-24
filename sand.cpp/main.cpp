6fb3b8e18744c6878d7f409d7c519e8e6f2e3c7f
hrbkcsq <hrbcksq@gmail.com>
2017-03-25 01:22:40 +0200

    Function definition and function declaration

--- sand.cpp/main.cpp
+++ sand.cpp/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int DemoConsoleOutput();
-
-int main() {
-	//cout << "Hello World!" << endl;
-	//<< std::endl
-	return DemoConsoleOutput();
-}
-
 int DemoConsoleOutput() {
 	cout << "This is a simple string literal" << endl;
 	cout << "Writing number fie: " << 5 << endl;
 	cout << "Perfoming division 10 / 5 = " << 10 / 5 << endl;
 	cout << "Pi when approximated is 22 / 7 =" << 22 / 7 << endl;
 	cout << "Pi more accurately is 22 / 7 = " << 22.0 / 7 << endl;
-
 	return 0;
-} 
+}
+
+int main() {
+	//cout << "Hello World!" << endl;
+	//<< std::endl
+	return DemoConsoleOutput();
+}
 
